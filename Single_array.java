afc9d6046d8e25e07e89e24d8b8713dac2f0b110
rommeljhon <jhonrommelcloma63@gmail.com>
2025-03-24 11:27:37 +0800

    add single and multidimentional array

---
+++ Single_array.java
@@ -0,0 +1,18 @@
+
+package midterm;
+
+
+public class Single_array {
+    
+    
+    public static void main(String[] args) {
+      
+
+String[] array = {"A", "B", "C", "D", "E", "F", "G"};
+
+for (String letter : array) {
+   System.out.print(letter + " , ");
+}
+   }
+}  
+
